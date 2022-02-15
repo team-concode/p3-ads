9ce0ce4dd75856a7a41e85169053ea691ba18555
mj.shin <killrain@gmail.com>
2022-02-15 18:00:32 +0900

    first commit

---
+++ src/settings.cda91.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Intro.fire",orientation:"",jsList:[],bundleVers:{internal:"468ae",resources:"c84c4",main:"3678e"}};
